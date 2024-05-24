d73aa4de217958d3eaada93880dd5c93eab0cd44
Luis Henrique <hicklourenco@gmail.com>
2024-05-24 14:26:36 -0300

    luis -- 24/05 -- 14:26

---
+++ pages/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../style/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav></nav>
+    <main>
+        <div class="container">
+            <div class="banner">
+                
+            </div>
+            <div class="login">
+            <form action="../database/login.act.php" method="post">
+                    <div class="title"><h1>Entre</h1></div>
+                    <div class="principal">
+
+                        <label for="email">email:</label>
+                        <input type="email" name="email">
+
+                        <label for="senha">Senha:</label>
+                        <input type="password" name="senha" id="senha" onkeyup="VerificarSenhas(senha.value,senha2.value)" require autocomplete="off">
+
+                    </div>
+                    <div class="rodape">
+                        <input type="submit" value="Entrar" class="btn">
+                        
+
+                        <?php
+                            @session_start();
+                            if (isset($_SESSION['msg'])) {
+                                echo $_SESSION['msg'];
+                                unset($_SESSION['msg']);
+                            }
+                        ?>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="../app/singin.js"></script>
+</body>
+</html>
