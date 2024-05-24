1b607f4a762c7bbe956ee6952f8f6447346e5c01
Luis Henrique <hicklourenco@gmail.com>
2024-05-24 17:02:34 -0300

    luis -- 24/05 -- 17:02

--- pages/signin.php
+++ pages/signin.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./libs/Mask-Plugin/src/jquery.mask.js"></script>
+    <script src="../libs/jquery.js"></script>
+    <script src="../libs/Mask-Plugin/src/jquery.mask.js"></script>
     <link rel="stylesheet" href="../style/signin.css">
     <title>Document</title>
 </head>
