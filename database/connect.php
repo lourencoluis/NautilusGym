625ab2c5b41ab37715d682aeaa6f14adeac9e4a8
Takara06 <kenititakara@gmail.com>
2024-06-20 18:58:30 -0300

    Delete database directory

--- database/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    if(@!$con = mysqli_connect('localhost','root','','bd_clientes')){
-        echo "Erro ao se conectar na base de dados!";
-    }
-    mysqli_query($con, "SET NAMES utf8");
--- database/sec.php
+++
@@ -1,9 +0,0 @@
-<?php
-    @session_start();
-
-    if(!isset($_SESSION['logado']) || $_SESSION['logado'] != true){
-        header("location:../pages/login.php");
-        $_SESSION['logado'] = false;
-    }
-
-
